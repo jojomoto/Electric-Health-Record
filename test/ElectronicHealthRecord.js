dffcdc85dbb2ccf470184e24d3464b94b69c412d
Joseph <115746225+jojomoto@users.noreply.github.com>
2022-11-01 11:17:46 -0700

    Update ElectronicHealthRecord.js

--- test/ElectronicHealthRecord.js
+++ test/ElectronicHealthRecord.js
@@ -1,36 +1,48 @@
-//Tests go here
-//Working progress
+
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-describe("ElectronicHealthRecord", function () {
-  it("Should assign the admin to the deploying address", async function () {
-    const [admin] = await ethers.getSigners();
 
-    
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+
+describe("EHR Smart Contract", function () {
+  async function deployTokenFixture() {  
     const ElectronicHealthRecord = await ethers.getContractFactory("ElectronicHealthRecord");
-
+    const [pharm, patient1, patient2, patient3, doctor1, doctor2] = await ethers.getSigners();
     const EHR = await ElectronicHealthRecord.deploy();
+    await EHR.deployed();
+    return { ElectronicHealthRecord, EHR, pharm, patient1, patient2, patient3, doctor1, doctor2 };
+  }
 
-    await EHR.viewAdmin();
-
-    expect(await EHR.viewAdmin()).to.equal(admin.address);
-  });
-  
-
-  it("Should add doctor if admin approves", async function () {
-    const [admin, doc] = await ethers.getSigners();
-
-    const ElectronicHealthRecord = await ethers.getContractFactory("ElectronicHealthRecord");
-
-    const EHR = await ElectronicHealthRecord.deploy();
-
-    await EHR.viewAdmin();
-
-    expect(await EHR.viewAdmin()).to.equal(admin.address);
-
-    await EHR.addDoctor(doc.address);
-
-    expect(await EHR.isDoctor(doc.address)).is.equal(true);
+  describe("Deployment", function () {
+   
+    it("Assign deployer to pharmacy address", async function () {
+      const { EHR, pharm } = await loadFixture(deployTokenFixture);  
+      expect(await EHR.viewPharmacy()).to.equal(pharm.address);
+    });
   });
 
+  describe("Patient Records", function () {
+
+    it("Patient registration", async function () {
+      const { EHR, patient1 } = await loadFixture(deployTokenFixture);
+      const patientInfo = await EHR.connect(patient1).registerPatient("patient1", 25);
+      expect(await EHR.connect(patient1).viewRecords()).to.deep.equal([patient1.address, 25, "patient1", [], []]);
+    });
+    /*
+    it("Patient View self only", async function () {
+      const { EHR, patient1, patient2 } = await loadFixture(deployTokenFixture);
+      const patientInfo = await EHR.connect(patient1).registerPatient("patient1", 25);
+      expect(await EHR.connect(patient2).viewRecords()).to.be.revertedWith("Not a registered patient");
+    });
+    */
+    it("Patient adds health issue and update age", async function () {
+      const { EHR, patient1 } = await loadFixture(deployTokenFixture);
+      const patientInfo = await EHR.connect(patient1).registerPatient("patient1", 25);
+      const patientMeds = await EHR.connect(patient1).addDisease("chronic migraine");
+      const patientAge = await EHR.connect(patient1).updateRecords(26);
+      expect(await EHR.connect(patient1).viewRecords()).to.deep.equal([patient1.address, 26, "patient1", ["chronic migraine"], []]);
+    })
+  });
+
+  describe("Doctor Records and Access")
 });
